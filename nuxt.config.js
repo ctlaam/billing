07fc644a236dc925c4bbb619826b6e3d2576c61c
ctlaam <caothanhlam14092000@gmail.com>
2023-07-20 17:28:30 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,26 @@ export default {
         href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css',
       },
     ],
+    script: [
+      {
+        src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+        integrity:
+          'sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo',
+        crossorigin: 'anonymous',
+      },
+      {
+        src: 'https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js',
+        integrity:
+          'sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1',
+        crossorigin: 'anonymous',
+      },
+      {
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js',
+        integrity:
+          'sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM',
+        crossorigin: 'anonymous',
+      },
+    ],
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
@@ -45,13 +65,13 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxtjs/fontawesome'
+    '@nuxtjs/fontawesome',
   ],
   fontawesome: {
     icons: {
       solid: true,
       regular: true,
-    }
+    },
   },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
